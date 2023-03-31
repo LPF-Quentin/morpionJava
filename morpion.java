fc15f461bac34c77d66cae353ebd128abeb12b22
LPF-Quentin <quentin-rousseau@orange.fr>
2023-03-31 14:43:46 +0200

    uwu

---
+++ morpion.java
@@ -0,0 +1,15 @@
+public static void main(String[] args) {
+    
+    gameBoard[][] = {
+        {'_','|','_','|','_'},
+        {'_','|','_','|','_'},
+        {'_','|','_','|','_'}
+    };
+
+    printBoard(gameBoard)
+
+}
+
+public static void printBoard(char [][] gameBoard){
+
+}
